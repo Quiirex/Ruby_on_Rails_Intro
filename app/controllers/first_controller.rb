4157bff75405fdac1ebc63231c00badc2365966c
Luka <j6.bountyhounter@gmail.com>
2020-04-30 10:55:35 +0200

    Update

--- app/controllers/first_controller.rb
+++ app/controllers/first_controller.rb
@@ -1,11 +1,8 @@
 class FirstController < ApplicationController
   def index
   end
-<<<<<<< HEAD
   def railsstrani
-=======
-
+  end
   def rubyintro
->>>>>>> a2d1e02b206926b0d80a1f9ebb18c47b36acfcce
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
   resources :subscribers
   #get 'first/index'
   root to: "first#index"
-<<<<<<< HEAD
-  get 'first#railsstrani'
-=======
-  root to: "first#rubyintro"
->>>>>>> a2d1e02b206926b0d80a1f9ebb18c47b36acfcce
+  #get 'first#railsstrani'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html¸
 end
