2b36002ad08b528f272f56540e479080cedcf8da
AmbicatusThe2nd <58732957+AmbicatusThe2nd@users.noreply.github.com>
2020-04-30 11:00:16 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :subscribers
   #get 'first/index'
   root to: "first#index"
-  #get 'first#railsstrani'
+  get 'first/rubyintro'
+  get 'first/railsstrani'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html¸
 end
