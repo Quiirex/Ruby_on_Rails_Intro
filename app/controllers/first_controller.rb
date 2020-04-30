50d8a19aeb797a56a1ee5d4ac7239b1cdcd4541e
KorosecTadej <46629006+KorosecTadej@users.noreply.github.com>
2020-04-30 10:49:45 +0200

    tado

--- app/controllers/first_controller.rb
+++ app/controllers/first_controller.rb
@@ -1,4 +1,11 @@
 class FirstController < ApplicationController
   def index
   end
+<<<<<<< HEAD
+  def railsstrani
+=======
+  
+  def rubyintro
+>>>>>>> a2d1e02b206926b0d80a1f9ebb18c47b36acfcce
+  end
 end
